5a8c4acb55438d0545192ff74fd070a52ce74404
Alex <temper8@gmail.com>
2021-07-13 18:31:35 +0300

    Update v4.f90

--- v4.f90
+++ v4.f90
@@ -1,32 +1,34 @@
 module Test
-  TYPE Vector4  
+
+  type Vector4  
     Complex,  Dimension(4) :: value
-  ENDTYPE Vector4
-  INTERFACE OPERATOR ( + )
-    MODULE PROCEDURE V_ADD
-  END INTERFACE
+  endtype Vector4
+
+  interface operator ( + )
+    module procedure V_ADD
+  end interface
 
 
 Contains
 
-  TYPE(Vector4) FUNCTION V_ADD(X,Y)
-  TYPE(Vector4), intent(IN) :: X, Y
-  V_ADD%value = X%value + Y%value
-  END FUNCTION V_ADD
+  type(Vector4) function V_ADD(X,Y)
+  type(Vector4), intent(IN) :: X, Y
+    V_ADD%value = X%value + Y%value
+  end function V_ADD
 
-  SUBROUTINE  FillVector(v)
-  TYPE(Vector4) v
+  subroutine  FillVector(v)
+    type(Vector4) v
     do i=1,4
       v%value(i) = (1.,1.)
     end do
-  end  SUBROUTINE  FillVector
+  end  subroutine  FillVector
 
 end module Test
 
 program v4
 ! This is a comment line, it is ignored by the compiler
   use Test
-TYPE(Vector4) v1, v2, v3
+  type(Vector4) v1, v2, v3
   print *, 'Hello, V4!'
   call FillVector(v1)
   call FillVector(v2)
