76c8c885a865cbed034af507dcf670b9da4614d8
Alex <temper8@gmail.com>
2021-07-15 10:31:39 +0300

    Create sub_matmul.f90

---
+++ sub_matmul.f90
@@ -0,0 +1,14 @@
+module sub
+contains 
+subroutine matmul(a,b,c,n)
+	real(8) a(n,n),b(n,n),c(n,n)
+	c=0.d0
+	do i=1,n         ! Outer loop is parallelized.
+	   do j=1,n      ! inner loops are interchanged
+		  do k=1,n   ! new inner loop is vectorized 
+			 c(j,i)=c(j,i)+a(k,i)*b(j,k)
+		  enddo
+	   enddo
+	enddo
+end
+end module sub
