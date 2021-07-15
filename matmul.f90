f49f8cee10675855b40e26b253c2ed4788cd5ff0
Alex <temper8@gmail.com>
2021-07-15 11:14:21 +0300

    Update matmul.f90

--- matmul.f90
+++ matmul.f90
@@ -1,6 +1,6 @@
 module mm
    Contains   
-   subroutine matmul(a,b,c,n)
+   subroutine matmul2(a,b,c,n)
       Real(8) a(n,n),b(n,n),c(n,n)
       c=0.d0
       do i=1,n         ! Outer loop is parallelized.
@@ -10,18 +10,30 @@ module mm
             enddo
          enddo
       enddo
-   end  subroutine  matmul   
+   end    
 end module mm
 
 program mul
    ! Vector4 tests
      use mm
+     use sub
      PARAMETER n = 3000
      real(8), dimension(n,n) :: a, b, c
      real    ::  TT1,TT2 
      print *, 'Hello, matmul'
      call cpu_time(T1) 
+     CALL RANDOM_NUMBER(a)
+     CALL RANDOM_NUMBER(b)    
+     call cpu_time(T2)
+     print *, "time=" , T2-T1
+
+     call cpu_time(T1) 
      call matmul(a,b,c,n)
+     call cpu_time(T2)
+     print *, "time=" , T2-T1
+
+     call cpu_time(T1) 
+     call matmul2(a,b,c,n)
      call cpu_time(T2)
      print *, "time=" , T2-T1
      print *,"end"
