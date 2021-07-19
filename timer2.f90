58e7c995bafb92243c9a5d032c2ac3c023f8c8f2
Alex <temper8@gmail.com>
2021-07-19 10:08:34 +0300

    Update timer2.f90

--- timer2.f90
+++ timer2.f90
@@ -1,28 +1,66 @@
-PROGRAM timer
+module test
+	Contains   
+	PURE subroutine test_f(nn)
+	 implicit none
+	   integer, intent (in) :: nn
+	   Real arr(nn,nn)
+	   integer i,j,x,y,n
+	   INTEGER , PARAMETER :: runs=10000
+	   x = nn
+	   y = nn
+	   DO n = 1 , runs
+		  !DIR$ PARALLEL  ALWAYS	
+		  FORALL(i = 1:x,j = 1:y)
+			 arr(j,i) = REAL(i)*REAL(j) + 2
+		  END FORALL
+		  arr(1,1) = arr(1,2)     
+	   END DO
+
+	end    
+ end module test
+
+PROGRAM timer2
+	use test
 	IMPLICIT NONE
 	REAL :: t1,t2,rate 
 	INTEGER :: c1,c2,cr,cm,i,j,n,s
 	INTEGER :: x, y, count
 	INTEGER , PARAMETER :: runs=10000
 	REAL, dimension (:,:), allocatable :: array
-  
+  	!dir$ attributes align:64 ::array
 	! First initialize the system_clock
 	CALL system_clock(count_rate=cr)
 	CALL system_clock(count_max=cm)
 	rate = REAL(cr)
 	WRITE(*,*) "system_clock rate ",rate
   
-	do count = 300,1000,100
-	x = count
-	y = count
-	allocate ( array(x,y) ) 	
+	do count = 100,500,100
+		x = count
+		y = count
+		
+		allocate ( array(x,y) )
+		CALL CPU_TIME(t1)
+		CALL SYSTEM_CLOCK(c1)
+
+		call test_f(count)
+
+		CALL CPU_TIME(t2)
+		CALL SYSTEM_CLOCK(c2)	
+		print *,x, x*y
+		WRITE(*,*) "system_clock : ",(c2 - c1)/rate
+		WRITE(*,*) "cpu_time     : ",(t2-t1)
+
+ 	
 	CALL CPU_TIME(t1)
 	CALL SYSTEM_CLOCK(c1)	
 	DO n = 1 , runs
-	   !DIR$ PARALLEL  ALWAYS	
-	   FORALL(i = 1:x,j = 1:y)
-		  array(i,j) = REAL(i)*REAL(j) + 2
-	   END FORALL
+
+	!DIR$ PARALLEL  ALWAYS
+	   do i = 1,x 
+		do j = 1,y
+		  array(j,i) = REAL(i)*REAL(j) + 2
+		end do
+		end do	
 	   array(1,1) = array(1,2)     
 	END DO   
 	CALL CPU_TIME(t2)
@@ -34,9 +72,9 @@ PROGRAM timer
 	CALL CPU_TIME(t1)
 	CALL SYSTEM_CLOCK(c1)	
 	DO n = 1 , runs
-	   !DIR$ NOPARALLEL	
+	   !DIR$ 	NOPARALLEL
 	   FORALL(i = 1:x,j = 1:y)
-		  array(i,j) = REAL(i)*REAL(j) + 2
+		  array(j,i) = REAL(i)*REAL(j) + 2
 	   END FORALL
 	   array(1,1) = array(1,2)     
 	END DO   
@@ -48,4 +86,4 @@ PROGRAM timer
 	print *,'-----------------'
 	deallocate (array)
 	end do
-  END PROGRAM timer
+  END PROGRAM timer2
