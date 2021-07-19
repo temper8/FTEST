7ad65ebe3fb30bbfb244f43b93383a9ee959ffe1
Alex <temper8@gmail.com>
2021-07-19 10:45:39 +0300

    Update timer2.f90

--- timer2.f90
+++ timer2.f90
@@ -23,7 +23,7 @@ PROGRAM timer2
 	use test
 	IMPLICIT NONE
 	REAL :: t1,t2,rate 
-	real sys_clock(3), cpu_time_(3)
+	real sys_clock(4), cpu_time_(4)
 	INTEGER :: c1,c2,cr,cm,i,j,n,s
 	INTEGER :: x, y, count
 	INTEGER , PARAMETER :: runs=10000
@@ -73,6 +73,23 @@ PROGRAM timer2
 	CALL SYSTEM_CLOCK(c1)	
 	DO n = 1 , runs
 	   !DIR$ 	NOPARALLEL
+		do i = 1,x 
+			do j = 1,y
+			  array(j,i) = REAL(i)*REAL(j) + 2
+			end do
+		end do
+	   array(1,1) = array(1,2)     
+	END DO   
+	CALL CPU_TIME(t2)
+	CALL SYSTEM_CLOCK(c2)	
+
+	sys_clock(3) = (c2 - c1)/rate
+	cpu_time_(3) = (t2-t1)
+
+	CALL CPU_TIME(t1)
+	CALL SYSTEM_CLOCK(c1)	
+	DO n = 1 , runs
+	   !DIR$ 	NOPARALLEL
 	   FORALL(i = 1:x,j = 1:y)
 		  array(j,i) = REAL(i)*REAL(j) + 2
 	   END FORALL
@@ -81,9 +98,10 @@ PROGRAM timer2
 	CALL CPU_TIME(t2)
 	CALL SYSTEM_CLOCK(c2)	
 
-	sys_clock(3) = (c2 - c1)/rate
-	cpu_time_(3) = (t2-t1)
-	print *, x, x*y, '        PARALLEL', '     NOPARALLEL'	
+	sys_clock(4) = (c2 - c1)/rate
+	cpu_time_(4) = (t2-t1)
+
+	print *, x, x*y, '         PARALLEL', '       NOPARALLEL', '     FORALL'	
 	WRITE(*,*) "system_clock : ",sys_clock
 	WRITE(*,*) "cpu_time     : ",cpu_time_
 	print *,'-----------------'
