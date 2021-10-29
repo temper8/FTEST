6eaf299c731d5f9933da853f9352047edab8f312
Alex <temper8@gmail.com>
2021-10-29 17:34:44 +0300

    move code to src folder

--- src/sub_matmul.f90
+++ src/sub_matmul.f90
@@ -3,6 +3,7 @@ contains
 subroutine matmul(a,b,c,n)
 	real(8) a(n,n),b(n,n),c(n,n)
 	c=0.d0
+	!DIR$ PARALLEL  ALWAYS
 	do i=1,n         ! Outer loop is parallelized.
 	   do j=1,n      ! inner loops are interchanged
 		  do k=1,n   ! new inner loop is vectorized 
