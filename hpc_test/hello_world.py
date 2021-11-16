8c86b81739b8535ecaaba5724484c6d4272c5584
Alex <temper8@gmail.com>
2021-11-16 11:56:39 +0300

    Update hello_world.py

--- hpc_test/hello_world.py
+++ hpc_test/hello_world.py
@@ -1,6 +1,9 @@
 import sys
-from mpi4py import MPI
-size = MPI.COMM_WORLD.Get_size()
-rank = MPI.COMM_WORLD.Get_rank()
-name = MPI.Get_processor_name()
-print("Hello, World! I am process ",rank," of ",size," on ",name)
+#from mpi4py import MPI
+#size = MPI.COMM_WORLD.Get_size()
+#rank = MPI.COMM_WORLD.Get_rank()
+#name = MPI.Get_processor_name()
+#print("Hello, World! I am process ",rank," of ",size," on ",name)
+print("Hello, World!")
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
