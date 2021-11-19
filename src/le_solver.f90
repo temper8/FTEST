2b818efa0dfa8858b7f745bb0d9e9653be7d889e
Alex <temper8@gmail.com>
2021-11-19 15:06:34 +0300

    create test_solver for linear equ

---
+++ src/le_solver.f90
@@ -0,0 +1,66 @@
+module le_solver
+    Contains   
+	subroutine PRINT_MATRIX( DESC, M, N, A, LDA )
+        CHARACTER*(*)    DESC
+        INTEGER          M, N, LDA
+        INTEGER          M1, N1
+        COMPLEX          A( LDA, * )
+      
+        INTEGER          I, J
+      
+        WRITE(*,*)
+        WRITE(*,*) DESC
+        M1 = M
+        N1 = N
+        if (M1>5) M1 = 5
+        if (N1>5) N1 = 5  
+        DO I = 1, M1
+           WRITE(*,9998) ( A( I, J ), J = 1, N1 )
+        END DO
+        
+  9998 FORMAT( 11(:,1X,'(',F6.2,',',F6.2,')') )
+      end  
+ 
+    subroutine mkl_le_solver(A, B)
+    !
+    ! Solve the complex equations A*X = B.
+    !
+    !     .. Parameters ..
+	    INTEGER          N, NRHS
+	    PARAMETER        ( NRHS = 1 )
+	    INTEGER          LDA, LDB
+
+    !     .. Local Scalars ..
+	    INTEGER          INFO
+	
+    !     .. Local Arrays ..
+	    INTEGER, dimension (:), allocatable ::   IPIV
+	    complex, dimension (:,:) :: A
+	    complex, dimension (:,:) :: B	
+
+	    integer, dimension( 4 )  ::	ISEED 
+
+        N = size(B)
+        !print *, N
+        LDA = N
+        LDB = N 
+   
+        ISEED = (/ 1, 2, 3, 4 /)
+
+        allocate ( IPIV(N) )
+
+        !CALL PRINT_MATRIX( 'A matrix', N, N, A, LDA )
+
+        !Solve the equations A*X = B.
+        CALL CGESV( N, NRHS, A, LDA, IPIV, B, LDB, INFO )
+    
+        IF( INFO.GT.0 ) THEN
+            WRITE(*,*)'The diagonal element of the triangular factor of A,'
+            WRITE(*,*)'U(',INFO,',',INFO,') is zero, so that'
+            WRITE(*,*)'A is singular; the solution could not be computed.'
+            STOP
+        END IF
+
+    end subroutine mkl_le_solver
+
+ end module le_solver
---
+++ src/test_solver.f90
@@ -0,0 +1,78 @@
+program test_solver
+    use utils
+    use le_solver
+    integer NMAX 
+    PARAMETER        ( NMAX = 10000 )
+    complex, dimension(NMAX,NMAX) :: RA, RB
+    
+	complex, dimension (:,:), allocatable :: A
+	complex, dimension (:,:), allocatable :: B	
+    
+	integer, dimension( 4 )  ::	ISEED
+
+    CHARACTER(len=32) :: arg
+    integer stat
+    integer nn
+
+    real all_time1, all_time2
+    real time_init
+    real time21, time22
+    real    ::  T1,T2 
+    real    ::  dt1, dt2, dt3 
+
+    print *, 'Solve the complex equations A*X = B.'
+    print *, 'and calc exec time'
+    CALL getarg(1, arg)
+    READ(arg,*,iostat=stat)N
+    if (stat<0) then
+       print *, "set default size"
+       N = 4
+    end if
+    print *, "N=", N 
+
+
+    
+    !allocate ( A(N, N) )
+    !allocate ( B(N, 1) )
+    
+
+    all_time1 = sys_time()
+    print *, 'Init matrix'
+    ISEED = (/ 1, 2, 3, 4 /)
+    call cpu_time(T1) 
+    call clarnv	(1, ISEED, NMAX*NMAX,	RA )	
+	call clarnv	(1, ISEED, NMAX,	RB )  
+    
+    print *, RB(1:N, 1:1)
+    !call clarnv	(1, ISEED, N*N,	A )	
+	!call clarnv	(1, ISEED, N,	B )  
+    
+    
+    call cpu_time(T2)
+    dt1 = T2-T1
+    print *, "init matrix time = ", dt1         
+    print *, "         n","   cpu time", "         sys time", "      cpu/sys time"    
+
+    do nn = 500, 10000, 500
+        N = nn
+        A = RA(1:N, 1:N)
+        B = RB(1:N, 1:1)
+        !print *, RB(1:N, 1:1)
+        time_init = sys_time()
+        call cpu_time(T1) 
+
+        call mkl_le_solver(A,B)
+
+
+        call cpu_time(T2)
+        time_matmul = sys_time() - time_init
+        dt2 = T2-T1
+    
+        print *, nn, dt2,  time_matmul, dt2/time_matmul
+
+       ! print *, RB(1:N, 1:1)
+    end do
+    all_time2 = sys_time()
+    print *,"all time =", all_time2 - all_time1
+
+end program test_solver
